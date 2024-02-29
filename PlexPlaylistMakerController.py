3de9913c2b7a7aa81d694f34313db25f6702969b
primetime43 <12754111+primetime43@users.noreply.github.com>
2024-02-29 14:02:39 -0500

    New UI design and moved backend to reflet MVC
    - More changes made to improve the UI design
    - Moved some code around to reflect MVC design

---
+++ PlexPlaylistMakerController.py
@@ -0,0 +1,136 @@
+from CTkMessagebox import CTkMessagebox
+import json
+import imdb
+import re
+import requests
+from bs4 import BeautifulSoup
+from threading import Thread
+from queue import Queue
+import webbrowser
+from plexapi.myplex import MyPlexPinLogin, MyPlexAccount
+import time
+from imdb import IMDbDataAccessError    
+ 
+class PlexIMDbApp:   
+    def login_and_fetch_servers(self, update_ui_callback, server_var):
+        headers = {'X-Plex-Client-Identifier': 'unique_client_identifier'}
+        pinlogin = MyPlexPinLogin(headers=headers, oauth=True)
+        oauth_url = pinlogin.oauthUrl()
+        webbrowser.open(oauth_url)
+        pinlogin.run(timeout=120)
+        pinlogin.waitForLogin()
+        if pinlogin.token:
+            plex_account = MyPlexAccount(token=pinlogin.token)
+            resources = [resource for resource in plex_account.resources() if resource.owned and resource.connections and resource.provides == 'server']
+            servers = [resource.name for resource in resources]
+            # Call the callback to update the GUI with the servers list
+            update_ui_callback(servers, server_var)
+            if servers:
+                self.server = plex_account.resource(servers[0]).connect()  # Make sure self.server is handled correctly
+        else:
+            CTkMessagebox(title="Error", message="Could not log in to Plex account", option_1="OK")
+        
+    def fetch_movie_details(self, queue, ia, imdb_id, retry_count=3, delay=1):
+        attempts = 0
+        while attempts < retry_count:
+            try:
+                movie = ia.get_movie(imdb_id[2:])  # Remove 'tt' prefix
+                title = movie.data.get('original title', movie.get('title'))
+                if title:
+                    queue.put((imdb_id, title))
+                    return
+            except IMDbDataAccessError as e:
+                print(f"Error fetching {imdb_id}: {e}. Attempt {attempts + 1} of {retry_count}")
+                time.sleep(delay * (attempts + 1))  # Exponential back-off
+                attempts += 1
+            except Exception as e:
+                print(f"Unexpected error fetching {imdb_id}: {e}")
+                return
+        print(f"Failed to fetch details for {imdb_id} after {retry_count} attempts.")
+        
+    def create_plex_playlist(self):
+        # Retrieve the IMDb List URL from the textbox
+        imdb_list_url = self.IMDB_covers_directory_textbox.get()
+        # Retrieve the Playlist name from the textbox
+        plex_playlist_name = self.IMDB_gamecache_textbox.get()
+
+        # Initialize cinemagoer IMDb interface
+        ia = imdb.Cinemagoer()
+
+        # Fetch IMDb list data
+        response = requests.get(imdb_list_url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find the JSON data within the page
+        json_str_match = re.search(r'<script type="application/ld\+json">(.+?)</script>', soup.prettify(), re.DOTALL)
+        imdb_movies = []
+        total_found_in_imdb_list = 0
+        if json_str_match:
+            json_str = json_str_match.group(1)
+            try:
+                data = json.loads(json_str)
+                items = data.get("about", {}).get("itemListElement", [])
+                total_found_in_imdb_list = len(items)  # Total number of movies in IMDb list
+                threads = []
+                queue = Queue()
+                for item in items:
+                    url = item.get("url", "")
+                    imdb_id_match = re.search(r'/title/(tt\d+)/', url)
+                    if imdb_id_match:
+                        imdb_id = imdb_id_match.group(1)
+                        thread = Thread(target=self.fetch_movie_details, args=(queue, ia, imdb_id))
+                        threads.append(thread)
+                        thread.start()
+
+                # Wait for all threads to complete
+                for thread in threads:
+                    thread.join()
+
+                # Collect all results
+                while not queue.empty():
+                    imdb_movies.append(queue.get())
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+                return
+
+        # Find the 'Movies' section in the Plex library and retrieve all movies
+        library = self.server.library.section('Movies')
+        all_movies = library.all()
+
+        movies_to_add = []
+        for imdb_id, title in imdb_movies:
+            matched_movies = library.search(title=title, libtype='movie')
+            for plex_movie in matched_movies:
+                # Matching based on title (maybe look into other attributes like year, etc. for better matching)
+                if title.lower() == plex_movie.title.lower():
+                    movies_to_add.append(plex_movie)
+                    break  # Break if a match is found to avoid adding duplicates
+
+        # Create the playlist with the matched movies
+        if movies_to_add:
+            self.server.createPlaylist(plex_playlist_name, items=movies_to_add)
+            success_message = f"Created playlist '{plex_playlist_name}' with {len(movies_to_add)} movies out of {total_found_in_imdb_list} found in the IMDb list."
+            CTkMessagebox(title="Success", message=success_message, icon="check", option_1="OK")
+        else:
+            error_message = f"No matching movies found in Plex library out of {total_found_in_imdb_list} movies in the IMDb list."
+            CTkMessagebox(title="Error", message=error_message, icon="cancel", option_1="OK")
+            
+def check_updates(version: str):
+    try:
+        rep_version = requests.get(
+            "URL_HERE"
+        ).text.strip()
+
+        try:
+            rep_version = float(rep_version)
+        except ValueError:
+            rep_version = version
+
+    except requests.exceptions.RequestException:
+        rep_version = version
+
+    return (
+        f"Plex Playlist Maker - {version}{' | NEW VERSION AVAILABLE' if version !=
+                                rep_version else ''}"
+    )
--- PlexPlaylistMakerGUI.py
+++ PlexPlaylistMakerGUI.py
@@ -1,30 +1,16 @@
 import customtkinter as ctk
-from CTkMessagebox import CTkMessagebox
 import tkinter as tk
-import json
-import imdb
-import re
 import os
 from PIL import Image
-import requests
-from bs4 import BeautifulSoup
-from threading import Thread
-from queue import Queue
-import webbrowser
-from plexapi.myplex import MyPlexPinLogin, MyPlexAccount
-import time
-from imdb import IMDbDataAccessError
+from PlexPlaylistMakerController import PlexIMDbApp, check_updates
 
 VERSION = 1.0
-
 
 class PlexPlaylistMakerGUI(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title(
-            f"Plex Playlist Maker - {VERSION}"
-        )
-        # self.check_updates(VERSION)
+        self.controller = PlexIMDbApp()
+        self.title(check_updates(VERSION))
         self.geometry("450x350")
         self.resizable(False, False)
         self.font = ("MS Sans Serif", 12, "bold")
@@ -81,6 +67,7 @@ class PlexPlaylistMakerGUI(ctk.CTk):
             anchor="w",
             image=self.Letterboxd_image,
             command=self.letterboxd_button_event,
+            state="disabled",
         )
         self.Letterboxd.grid(row=2, column=0, sticky="ew")
 
@@ -233,30 +220,16 @@ class PlexPlaylistMakerGUI(ctk.CTk):
     def imdb_button_event(self):
         self.select_frame_by_name("imdb_frame")
         # Call login_and_fetch_servers to populate the servers dropdown
-        self.login_and_fetch_servers()
+        self.controller.login_and_fetch_servers(self.update_server_dropdown, self.server_var)
 
     def letterboxd_button_event(self):
-        self.select_frame_by_name("letterboxd_frame")
+        self.controller.select_frame_by_name("letterboxd_frame")
         
-    #move this function eventually    
-    def login_and_fetch_servers(self):
-        headers = {'X-Plex-Client-Identifier': 'unique_client_identifier'}  # Replace with your unique identifier
-        pinlogin = MyPlexPinLogin(headers=headers, oauth=True)
-        oauth_url = pinlogin.oauthUrl()
-        webbrowser.open(oauth_url)
-        pinlogin.run(timeout=120)
-        pinlogin.waitForLogin()
-        if pinlogin.token:
-            plex_account = MyPlexAccount(token=pinlogin.token)
-            resources = [resource for resource in plex_account.resources() if resource.owned and resource.connections and resource.provides == 'server']
-            servers = [resource.name for resource in resources]
-            self.server_var.set(servers[0] if servers else "")
-            self.update_server_option_menu(servers)  # Update the option menu
-            if servers:
-                self.server = plex_account.resource(servers[0]).connect()
-        else:
-            CTkMessagebox(title="Error", message="Could not log in to Plex account", option_1="OK")
-            
+    def update_server_dropdown(self, servers, server_var):
+        # Update the server dropdown based on the fetched servers
+        server_var.set(servers[0] if servers else "")
+        self.update_server_option_menu(servers)
+
     def update_server_option_menu(self, server_names):
         # Destroy the existing option menu widget, if it exists
         if hasattr(self, 'IMDB_server_menu'):
@@ -269,112 +242,6 @@ class PlexPlaylistMakerGUI(ctk.CTk):
             values=server_names  # Pass the updated list of server names here
         )
         self.IMDB_server_menu.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-        
-    def fetch_movie_details(self, queue, ia, imdb_id, retry_count=3, delay=1):
-        attempts = 0
-        while attempts < retry_count:
-            try:
-                movie = ia.get_movie(imdb_id[2:])  # Remove 'tt' prefix
-                title = movie.data.get('original title', movie.get('title'))
-                if title:
-                    queue.put((imdb_id, title))
-                    return
-            except IMDbDataAccessError as e:
-                print(f"Error fetching {imdb_id}: {e}. Attempt {attempts + 1} of {retry_count}")
-                time.sleep(delay * (attempts + 1))  # Exponential back-off
-                attempts += 1
-            except Exception as e:
-                print(f"Unexpected error fetching {imdb_id}: {e}")
-                return
-        print(f"Failed to fetch details for {imdb_id} after {retry_count} attempts.")
-        
-    def create_plex_playlist(self):
-        # Retrieve the IMDb List URL from the textbox
-        imdb_list_url = self.IMDB_covers_directory_textbox.get()
-        # Retrieve the Playlist name from the textbox
-        plex_playlist_name = self.IMDB_gamecache_textbox.get()
-
-        # Initialize cinemagoer IMDb interface
-        ia = imdb.Cinemagoer()
-
-        # Fetch IMDb list data
-        response = requests.get(imdb_list_url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Find the JSON data within the page
-        json_str_match = re.search(r'<script type="application/ld\+json">(.+?)</script>', soup.prettify(), re.DOTALL)
-        imdb_movies = []
-        total_found_in_imdb_list = 0
-        if json_str_match:
-            json_str = json_str_match.group(1)
-            try:
-                data = json.loads(json_str)
-                items = data.get("about", {}).get("itemListElement", [])
-                total_found_in_imdb_list = len(items)  # Total number of movies in IMDb list
-                threads = []
-                queue = Queue()
-                for item in items:
-                    url = item.get("url", "")
-                    imdb_id_match = re.search(r'/title/(tt\d+)/', url)
-                    if imdb_id_match:
-                        imdb_id = imdb_id_match.group(1)
-                        thread = Thread(target=self.fetch_movie_details, args=(queue, ia, imdb_id))
-                        threads.append(thread)
-                        thread.start()
-
-                # Wait for all threads to complete
-                for thread in threads:
-                    thread.join()
-
-                # Collect all results
-                while not queue.empty():
-                    imdb_movies.append(queue.get())
-            except json.JSONDecodeError as e:
-                print(f"Error decoding JSON: {e}")
-                return
-
-        # Find the 'Movies' section in the Plex library and retrieve all movies
-        library = self.server.library.section('Movies')
-        all_movies = library.all()
-
-        movies_to_add = []
-        for imdb_id, title in imdb_movies:
-            matched_movies = library.search(title=title, libtype='movie')
-            for plex_movie in matched_movies:
-                # Matching based on title (maybe look into other attributes like year, etc. for better matching)
-                if title.lower() == plex_movie.title.lower():
-                    movies_to_add.append(plex_movie)
-                    break  # Break if a match is found to avoid adding duplicates
-
-        # Create the playlist with the matched movies
-        if movies_to_add:
-            self.server.createPlaylist(plex_playlist_name, items=movies_to_add)
-            success_message = f"Created playlist '{plex_playlist_name}' with {len(movies_to_add)} movies out of {total_found_in_imdb_list} found in the IMDb list."
-            CTkMessagebox(title="Success", message=success_message, icon="check", option_1="OK")
-        else:
-            error_message = f"No matching movies found in Plex library out of {total_found_in_imdb_list} movies in the IMDb list."
-            CTkMessagebox(title="Error", message=error_message, icon="cancel", option_1="OK")
-
-    def check_updates(self, version: str):
-        try:
-            rep_version = requests.get(
-                "URL_HERE"
-            ).text.strip()
-
-            try:
-                rep_version = float(rep_version)
-            except ValueError:
-                rep_version = version
-
-        except requests.exceptions.RequestException:
-            rep_version = version
-
-        self.title(
-            f"Plex Playlist Maker - {version}{' | NEW VERSION AVAILABLE' if version !=
-                                    rep_version else ''}"
-        )
-
 
 if __name__ == "__main__":
     app = PlexPlaylistMakerGUI()
